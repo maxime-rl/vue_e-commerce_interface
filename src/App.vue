6b37357eaf92120007ad8bbe8ac4cdf83a8a5885
maxime-rl <maxime.robil.lepretre@gmail.com>
2020-11-16 02:01:11 +0100

    header structure

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-
+    <the-header></the-header>
   </div>
 </template>
 
 <script>
-
+import TheHeader from './components/TheHeader.vue'
 
 export default {
   name: 'App',
   components: {
-  
+    TheHeader
   }
 }
 </script>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Logo
+    <!--<img src="../assets/logo.png" alt="">-->
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Boutique</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">admin</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</template>
+
+
+<script>
+
+export default {
+}
+
+</script>
+
+
+<style lang="scss" scoped>
+nav a img {
+  width: 40px;
+  height: 40px;
+}
+</style>
