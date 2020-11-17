0a792169d59e1da62f52d711e6a18086b977cd5e
maxime-rl <maxime.robil.lepretre@gmail.com>
2020-11-17 15:44:20 +0100

    navigation integration with vue-router

--- src/App.vue
+++ src/App.vue
@@ -2,36 +2,19 @@
   <div id="app">
     <the-header></the-header>
     <div class="d-flex flex-column w-100">
-      <component :is="page"></component> <!-- composant dynamique -->
+      <router-view></router-view>
     </div>
   </div>
 </template>
 
 <script>
-import { eventBus } from './main';
-
 import TheHeader from './components/TheHeader';
-import User from './components/features/User/User';
-import Admin from './components/features/Admin/Admin';
-
 
 export default {
   name: 'App',
   components: {
     // export components
     TheHeader,
-    User,
-    Admin
-  },
-  data() {
-    return {
-      page: eventBus.page
-    }
-  },
-  created() { // on ecoute les changements de attribut .page de eventBus
-    eventBus.$on('update:page', (page) => { // mise a jour de la propriete .page
-      this.page = page;
-    })
   }
 }
 </script>
